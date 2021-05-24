aa185294c5bbe3fa6289ad24bf2a7712e886ac6b
Ayush Kumar <kayush242@gmail.com>
2021-05-24 17:09:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import MaterialTable from 'material-table';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TableDesign from "./component/TableDesign";
 
-function App() {
-  const [data, setData] = useState([]);
-  const columns = [
-    { title: 'ID', field: 'id' },
-    { title: 'Username', field: 'username' },
-    { title: 'Name', field: 'name' },
-    { title: 'Email', field: 'email' },
-    { title: 'Phone', field: 'phone' },
-    { title: 'Web Link', field: 'website' },
-  ];
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((resp) => resp.json())
-      .then((resp) => {
-        setData(resp);
-      });
-  }, []);
+const App = () => {
 
   return (
-    <div className="App">
-      <h1 align="center">React-App</h1>
-      <h4 align="center">Material Table</h4>
-      <MaterialTable title="Employee Data" data={data} columns={columns} />
+    <div>
+      <TableDesign />;
     </div>
   );
+
 }
+
 
 export default App;
